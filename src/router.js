0de41769f5f4ee2523d0a959c99580b101b25e8e
aman halabyar <amanhalabyar7@gmail.com>
2019-05-03 00:22:32 +0300

    edit the index.js and the router.js

---
+++ src/router.js
@@ -0,0 +1,15 @@
+const { handlerHome, handlerSearch, handlerPublic } = require("./handler");
+
+const router = (request, response) => {
+  const endpoint = request.url;
+
+  if(endpoint === "/") {
+    handlerHome(request, response);
+  } else if (endpoint === "/search") {
+    handlerSearch(request, response);
+  } else {
+    handlerPublic(request, response);
+  }
+};
+
+module.exports = router;
